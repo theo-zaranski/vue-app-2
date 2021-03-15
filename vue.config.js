58fecff713e1298260f8bc04d8d7b4d2131a3974
theo.zaranski <theo.zaranski@epsi.fr>
2021-03-15 09:17:14 +0100

    modif vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,16 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ]
-},{
+}
+
+module.exports = {
+
   outputDir: 'docs',
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-app-2/'
-    : '/'
+
+  publicPath: process.env.NODE_ENV=== 'production'
+
+      ? '/vue-app-2/'
+
+      : '/'
+
 }
